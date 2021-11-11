5524e16c93106c318a5e1cae931f55e072004f2c
panexxo <88559415+panexxo@users.noreply.github.com>
2021-11-11 14:49:18 -0500

    updatePHPPstgresConnection

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,18 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+  print "Hello, World! php version is " . PHP_VERSION . "\n";
+
+  $host        = $_ENV["INFODB_SERVICE_HOST"];
+  $port        = "";
+  $dbname      = $_ENV["POSTGRESQL_DATABASE"];
+  $user        = $_ENV["POSTGRESQL_USER"];
+  $password    = $_ENV["POSTGRESQL_PASSWORD"];
+
+  print " Host: " .$_ENV["INFODB_SERVICE_HOST"].  " DBname:" .$_ENV["POSTGRESQL_DATABASE"]. " User:" .$_ENV["POSTGRESQL_USER"]. " Password:" . $_ENV["POSTGRESQL_PASSWORD"]. "<br>";
+
+  if (!$connection = pg_connect ("host=$host dbname=$dbname user=$user password=$password")) {
+      $error = error_get_last();
+      echo "Connection failed. Error was: ". $error['message']. "\n";
+  } else {
+      echo "Connection succesful.\n";
+  }
 ?>
